9f5fc31c2289baecff58042ff02bbb0ef3f0f241
Justin Eldridge <eldridgejm@gmail.com>
2023-11-19 21:53:00 -0800

    Added 'transform_path' to copy_images.

--- panprob/postprocessors/_images.py
+++ panprob/postprocessors/_images.py
@@ -1,11 +1,18 @@
+import copy
 import pathlib
 import shutil
+import typing
 
 from ..ast import Node, InternalNode, ImageFile
 
 
-def copy_images(node: Node, src: pathlib.Path, dest: pathlib.Path) -> None:
-    """Copy the images referenced by ImageFile nodes to the destination directory.
+def copy_images(
+    node: Node,
+    src: pathlib.Path,
+    dest: pathlib.Path,
+    transform_path: typing.Callable[[str], str] = lambda x: x,
+) -> Node:
+    """Copy the images referenced by :class:`ImageFile` nodes to the destination directory.
 
     Parameters
     ----------
@@ -17,13 +24,27 @@ def copy_images(node: Node, src: pathlib.Path, dest: pathlib.Path) -> None:
     dest : pathlib.Path
         The path to the directory where the files should be copied to. All
         necessary directories will be created.
+    transform_path : typing.Callable[[str], str], optional
+        A function that, when called on the image's original relative path,
+        returns a new relative path that will be used as the images new path,
+        relative to the output directory. The AST will be updated to reflect
+        this change. The default is the identity function.
 
     """
     if isinstance(node, ImageFile):
+        # copy the image
         src_path = src / node.relative_path
-        dest_path = dest / node.relative_path
+        dest_path = dest / transform_path(node.relative_path)
         dest_path.parent.mkdir(parents=True, exist_ok=True)
         shutil.copyfile(src_path, dest_path)
+
+        # update the AST
+        node = copy.copy(node)
+        node.relative_path = transform_path(node.relative_path)
     elif isinstance(node, InternalNode):
-        for child in node.children:
-            copy_images(child, src, dest)
+        node = copy.copy(node)
+        node.children = tuple(
+            copy_images(child, src, dest, transform_path=transform_path)
+            for child in node.children
+        )
+    return node
--- test/test_copy_images.py
+++ test/test_copy_images.py
@@ -43,3 +43,74 @@ def test_copy_images(tmpdir):
     assert (dest / "foo" / "image_2.png").exists()
     assert (dest / "image_1.png").open().read() == "image 1"
     assert (dest / "foo" / "image_2.png").open().read() == "image 2"
+
+
+# path transformation ==================================================================
+
+
+def test_path_transformation_changes_destination_file_name(tmpdir):
+    source = pathlib.Path(tmpdir / "source")
+    source.mkdir()
+    dest = pathlib.Path(tmpdir / "dest")
+    dest.mkdir()
+
+    with (source / "image_1.png").open("w") as fileob:
+        fileob.write("image 1")
+
+    tree = ast.Problem(
+        children=[
+            ast.Paragraph(
+                children=[
+                    ast.Text("This is an image:"),
+                ]
+            ),
+            ast.ImageFile("image_1.png"),
+        ]
+    )
+
+    def transformer(relpath):
+        """Adds "foo-" as a prefix to the file name."""
+        return "foo-" + relpath
+
+    copy_images(tree, src=source, dest=dest, transform_path=transformer)
+
+    assert (dest / "foo-image_1.png").exists()
+    assert (dest / "foo-image_1.png").open().read() == "image 1"
+
+
+def test_path_transformation_changes_ast(tmpdir):
+    source = pathlib.Path(tmpdir / "source")
+    source.mkdir()
+    dest = pathlib.Path(tmpdir / "dest")
+    dest.mkdir()
+
+    with (source / "image_1.png").open("w") as fileob:
+        fileob.write("image 1")
+
+    tree = ast.Problem(
+        children=[
+            ast.Paragraph(
+                children=[
+                    ast.Text("This is an image:"),
+                ]
+            ),
+            ast.ImageFile("image_1.png"),
+        ]
+    )
+
+    def transformer(relpath):
+        """Adds "foo-" as a prefix to the file name."""
+        return "foo-" + relpath
+
+    tree = copy_images(tree, src=source, dest=dest, transform_path=transformer)
+
+    assert tree == ast.Problem(
+        children=[
+            ast.Paragraph(
+                children=[
+                    ast.Text("This is an image:"),
+                ]
+            ),
+            ast.ImageFile("foo-image_1.png"),
+        ]
+    )
